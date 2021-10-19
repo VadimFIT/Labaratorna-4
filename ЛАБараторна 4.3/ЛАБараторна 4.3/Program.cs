5c9b0959fb9662baa9c55b46f222febeca0468d7
VadimFIT <V.Kurdya.FIT.121.20@knute.edu.ua>
2021-10-19 14:25:18 +0300

    laba 4

---
+++ ЛАБараторна 4.3/ЛАБараторна 4.3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ЛАБараторна_4._3
+{
+    class VadymInfo
+    {
+        public string name; 
+        public int age;
+        public string car;
+        public VadymInfo() { name = "Vadym"; age = 19; car = "Nissan"; }
+        public void Infomation()
+        {
+            Console.WriteLine($"Имя: {name}  Возраст: {age} Авто: {car}");
+        }
+    }
+
+    class ArtemInfo
+    {
+        public string name;
+        public int age;
+        public string moto;
+        public ArtemInfo() { name = "Artem"; age = 21; moto = "BMW"; }
+        public void Infomation()
+        {
+            Console.WriteLine($"Имя: {name}  Возраст: {age} Мотоцикл: {moto}");
+        }
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VadymInfo vadyainf = new VadymInfo();
+            ArtemInfo arteminf = new ArtemInfo();
+           
+
+            vadyainf.Infomation();
+            arteminf.Infomation();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Лабараторна 4.1/Лабараторна 4.1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Лабараторна_4._1
+{
+    class ColorWhite
+    {
+        public virtual void Print(string value)
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.WriteLine(value);
+        }
+ 
+    }
+ 
+    class ColorGreen : ColorWhite
+    {
+        public override void Print(string value)
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine(value);
+        }
+    }
+ 
+    class ColorRed : ColorWhite
+    {
+        public new void Print(string value)
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine(value);
+        }
+ 
+    }
+ 
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            var arr = new ColorWhite[3];
+            arr[0] = new ColorWhite();
+            arr[1] = new ColorRed();
+            arr[2] = new ColorGreen();
+
+           
+            ((ColorWhite)arr[0]).Print("White");
+            ((ColorRed)arr[1]).Print("Red");
+            ((ColorGreen)arr[2]).Print("Blue");
+
+        }
+
+    }
+}
